03e170047262ebe5e704fada84302bce0df95b4e
Eric Morales <ericdaniel3103@gmail.com>
2022-01-18 15:19:09 -0600

    Añadiendo JS para mostrar el formulario

---
+++ src/js/tareas.js
@@ -0,0 +1,9 @@
+(function () {
+    // Botón para mostrar el Modal Agregar Tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        console.log('Mostrando formulario');
+    }
+})();
