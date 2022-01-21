f76496952d59c3713e7781dcae1c0f3e110156c9
Eric Morales <ericdaniel3103@gmail.com>
2022-01-20 18:54:58 -0600

    CSS al Listado de Tareas

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -69,6 +69,8 @@
 
             const listadoTareas = document.querySelector('#listado-tareas');
             listadoTareas.appendChild(contenedorTarea);
+
+            console.log(listadoTareas);
         })
     }
 
