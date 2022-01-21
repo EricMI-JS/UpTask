594350c990c371ffb716808ecf0fed570c451c48
Eric Morales <ericdaniel3103@gmail.com>
2022-01-20 18:29:34 -0600

    Agregando las tareas a la interfaz

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -3,9 +3,8 @@
 <div class="contendor-sm">
     <div class="contenedor-nueva-tarea">
         <button type="button" class="agregar-tarea" id="agregar-tarea">&#43; Nueva Tarea</button>
-
-        <ul id="listado-tareas" class="listado-tareas"></ul>
     </div>
+    <ul id="listado-tareas" class="listado-tareas"></ul>
 </div>
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
